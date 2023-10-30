ac00b4b06a9be37793babc5ef6a371cb9b9e6217
Anish Agrawal <114282241+pacd321@users.noreply.github.com>
2023-10-30 22:47:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
 ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
 ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
-
+#SRM
 const typed = new Typed('.multiple-text',{
     strings:['CSE Student at SRM University ','Frontend developer'],
     typeSpeed:100,
